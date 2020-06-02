74a4f176d5c1d482135c47590b53d8903c139a52
tej900 <66294812+tej900@users.noreply.github.com>
2020-06-02 16:02:55 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -3,18 +3,10 @@ for i in range(1,11):
 print("hi there")
 print("yes")
 print("this change i made on remote server")
-
 print("only local server is changed!!")
-
 print("booo")
-
-
 a=11
-
-
-
 a=12
-
 a=12
 b=17
 
